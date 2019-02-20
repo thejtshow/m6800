304e97ef0f35fa4f237f5a648953be1b32b59037
thejtshow <jtdavis777@gmail.com>
2019-02-20 13:12:44 -0500

    Changed get_instruction_text

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ import struct
 
 from binaryninja import (
     Architecture, RegisterInfo, FlagRole, LowLevelILFlagCondition, log_error, InstructionTextToken,
-    InstructionTextTokenType, InstructionInfo, BranchType
+    InstructionTextTokenType as ITTT, InstructionInfo, BranchType
 )
 
 # pylint: disable=wildcard-import
@@ -32,11 +32,10 @@ class M6800(Architecture):
         'V': FlagRole.OverflowFlagRole,
         'Z': FlagRole.ZeroFlagRole,
         'N': FlagRole.NegativeSignFlagRole,
-        'I': FlagRole.SpecialFlagRole,  # Interrupt Flag
-        'H': FlagRole.HalfCarryFlagRole
+        'I': FlagRole.SpecialFlagRole,      # Interrupt Flag
+        'H': FlagRole.HalfCarryFlagRole     # may be able to ignore -- no branch uses it
     }
 
-    # TODO: figure out how half-carry fits into this
     flags_required_for_flag_condition = {
         LowLevelILFlagCondition.LLFC_UGE: ['C'],
         LowLevelILFlagCondition.LLFC_UGT: ['C', 'Z'],
@@ -56,81 +55,78 @@ class M6800(Architecture):
     def _decode_instruction(data, addr):
         opcode = data[0]
         try:
-            nmemonic, inst_length, accumulator, mode = INSTRUCTIONS[opcode]
+            nmemonic, inst_length, accumulator, inst_type, mode = INSTRUCTIONS[opcode]
         except KeyError:
             raise LookupError(f'Opcode 0x{opcode:X} at address 0x{addr:X} is invalid.')
 
-        true_location = None
-        false_location = None
+        value = None
 
-        if opcode in CALL_INSTRUCTIONS + JMP_INSTRUCTIONS + BRANCH_INSTRUCTIONS:
+        # need to collect information based on each address mode
+        # INHERENT addressing => value is None
+        # ACCUMULATOR addressing => value is in accumulator
+        if mode == AddressMode.RELATIVE:
+            # should always be 2 bytes long, second byte is 2's complement
+            value = addr + inst_length + data[1].to_bytes(1, 'big', signed=True)
+        elif mode == AddressMode.IMMEDIATE:
             if inst_length == 2:
-                true_location = addr + 2 + data[1]
+                value = data[1]
             else:
-                true_location = struct.unpack('>H', data[1:3])[0]
-            if opcode in BRANCH_INSTRUCTIONS:  # conditionals have a false location
-                false_location = addr + inst_length
-        return (opcode, nmemonic, inst_length, accumulator, mode, true_location, false_location)
+                value = struct.unpack('>H', data[1:3])[0]
+        elif mode == AddressMode.EXTENDED:
+            value = struct.unpack('>H', data[1:3])[0]
+        elif mode in [AddressMode.INDEXED,
+                      AddressMode.DIRECT]:
+            value = data[1]
+
+        return nmemonic, inst_length, accumulator, inst_type, mode, value
 
     def get_instruction_text(self, data, addr):
         try:
-            (_, nmemonic, inst_length,
-             accumulator, mode, true_location, _) = M6800._decode_instruction(data, addr)
+            (nmemonic, inst_length, accumulator,
+             _, mode, value) = M6800._decode_instruction(data, addr)
         except LookupError as error:
             log_error(error.__str__())
             return None
 
         tokens = [
-            InstructionTextToken(InstructionTextTokenType.TextToken, nmemonic)
+            InstructionTextToken(ITTT.InstructionToken, nmemonic)
         ]
-        if accumulator:
-            tokens.append(
-                InstructionTextToken(InstructionTextTokenType.OperandSeparatorToken, ' ')
-            )
-            tokens.append(
-                InstructionTextToken(InstructionTextTokenType.RegisterToken, accumulator)
-            )
-        if true_location:
-            tokens.append(
-                InstructionTextToken(InstructionTextTokenType.OperandSeparatorToken, ' ')
-            )
-            if mode != AddressMode.INDEXED:
-                tokens.append(
-                    InstructionTextToken(
-                        InstructionTextTokenType.PossibleAddressToken, f'{true_location:X}'
-                    )
-                )
-            else:
-                tokens.append(
-                    InstructionTextToken(
-                        InstructionTextTokenType.TextToken, 'INDEXED'
-                    )
-                )
+
+        if mode == AddressMode.ACCUMULATOR:
+            tokens.append(InstructionTextToken(ITTT.OperandSeparatorToken, ' '))
+            tokens.append(InstructionTextToken(ITTT.RegisterToken, accumulator))
+        elif mode in [AddressMode.DIRECT, AddressMode.EXTENDED, AddressMode.RELATIVE]:
+            tokens.append(InstructionTextToken(ITTT.OperandSeparatorToken, ' '))
+            tokens.append(InstructionTextToken(ITTT.BeginMemoryOperandToken, '['))
+            tokens.append(InstructionTextToken(ITTT.PossibleAddressToken, f'0x{value:X}', value))
+            tokens.append(InstructionTextToken(ITTT.EndMemoryOperandToken, ']'))
+        elif mode == AddressMode.IMMEDIATE:
+            if accumulator:
+                tokens.append(InstructionTextToken(ITTT.OperandSeparatorToken, ' '))
+                tokens.append(InstructionTextToken(ITTT.RegisterToken, accumulator))
+            tokens.append(InstructionTextToken(ITTT.OperandSeparatorToken, ' '))
+            tokens.append(InstructionTextToken(ITTT.IntegerToken, f'0x{value:X}', value))
+        elif mode == AddressMode.INDEXED:
+            if accumulator:
+                tokens.append(InstructionTextToken(ITTT.OperandSeparatorToken, ' '))
+                tokens.append(InstructionTextToken(ITTT.RegisterToken, accumulator))
+            tokens.append(InstructionTextToken(ITTT.OperandSeparatorToken, ' '))
+            tokens.append(InstructionTextToken(ITTT.RegisterToken, 'IX'))
+            tokens.append(InstructionTextToken(ITTT.OperandSeparatorToken, ' + '))
+            tokens.append(InstructionTextToken(ITTT.IntegerToken, f'0x{value:X}', value))
+
         return tokens, inst_length
 
     def get_instruction_info(self, data, addr):
         try:
-            (opcode, _, inst_length,
-             _, mode, true_location, false_location) = M6800._decode_instruction(data, addr)
+            (nmemonic, inst_length, accumulator,
+             inst_type, mode, value) = M6800._decode_instruction(data, addr)
         except LookupError as error:
             log_error(error.__str__())
             return None
 
         inst = InstructionInfo()
         inst.length = inst_length
-
-        if (opcode in BRANCH_INSTRUCTIONS + CALL_INSTRUCTIONS + JMP_INSTRUCTIONS
-                and mode == AddressMode.INDEXED):
-            inst.add_branch(BranchType.UnresolvedBranch)
-        elif opcode in BRANCH_INSTRUCTIONS:
-            inst.add_branch(BranchType.TrueBranch, true_location)
-            inst.add_branch(BranchType.FalseBranch, false_location)
-        elif opcode in CALL_INSTRUCTIONS:
-            inst.add_branch(BranchType.CallDestination, true_location)
-        elif opcode in JMP_INSTRUCTIONS:
-            inst.add_branch(BranchType.UnconditionalBranch, true_location)
-        elif opcode in RETURN_INSTRUCTIONS:
-            inst.add_branch(BranchType.FunctionReturn)
 
         return inst
 
